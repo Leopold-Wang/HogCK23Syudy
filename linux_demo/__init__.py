c981c2307355687745b1948549b818c278bb292d
wzg <abandoned@126.com>
2022-03-30 14:22:56 +0800

    add python_demo

---
+++ linux_demo/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time: 2022/3/28 10:43
+# @Author: WZG
+# @File: __init__.py.py
---
+++ python_demo/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time: 2022/3/30 11:58
+# @Author: WZG
+# @File: __init__.py.py
